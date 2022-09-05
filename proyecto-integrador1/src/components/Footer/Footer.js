c5acd65e4bfd82ef8eb0b0be8cab6976d205f094
tsanzbelloni <tsanzbelloni@udesa.edu.ar>
2022-09-05 12:12:40 -0300

    cambios en headther y footer. Creamos la screens de SearchResults

--- proyecto-integrador1/src/components/Footer/Footer.js
+++ proyecto-integrador1/src/components/Footer/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './footer.css';
 
 function Footer
 () {
   return (
 
 <footer>
-<p>Copyright © Tomás Sanz Belloni, Carmen Rodríguez Reynoso y María Paz Russell - Programación III primavera 2022</p>
-</footer>    );
+        <p class="integrantes"> Copyright © Tomás Sanz Belloni, Carmen Rodríguez Reynoso y María Paz Russell - Programación III primavera 2022</p>
+    </footer>    
+
+);
 }
 
 export default Footer
--- proyecto-integrador1/src/components/Header/Header.js
+++ proyecto-integrador1/src/components/Header/Header.js
@@ -4,33 +4,45 @@ import './header.css';
 
 function Header (){
     return (
-        <header>
-            <nav className='main-nav'>
-                    <ul>
-                    <li>
-                        <Link to='/'>Home</Link>
-                    </li>
+    <header>
+        
+    <section className="logo">
+        <div className="imagenes-logo">
+            <img src="imagenes/anytime-slogan.PNG" alt="Music Anytime" className="slogan"/>
+            <img src="imagenes/logo3.PNG" alt="Logo Planet Music"/>
+            <img src="imagenes/anywhere-slogan.PNG" alt="Music Anywhere" className="slogan"/>
+        </div>
+        <div className="alerta">
+            <p className="probando1">The field can't be empty</p>
+            <p className="probando2">You should enter at least three characters</p>
+        </div>  
+    </section>
+    <nav className="navegador">
+        <img src="imagenes/logo3.PNG" alt="Logo Planet Music"/>
+        <form action="search-results.html" method="GET" className="search">
+            <input type="text" name="search" value="" placeholder="Search..."/>
+            <button type="submit"><i className="fa fa-search"></i></button>
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </form>
+        <ul className="Navegador">
 
-                    <li>
-                    <Link to='/categories'>Categories</Link>
-                    </li>
+        <li> <Link to='/'>Home</Link></li>
+        <li><Link to='/categories'>Categories</Link></li>
+        <li><Link to='/details'>Details</Link></li>    
+        <li><Link to='/favorites'>Favorites</Link></li>
+        <li className='usuario'>Usuario </li>
+        
+        </ul>
 
-                    <li>
-                    <Link to='/details'>Details</Link>
-                    </li>
-                    
-                    <li>
-                        <Link to='/favorites'>Favorites</Link>
-                    </li>
-                </ul>
 
-                <ul class="user">
-            <li>Nombre usuario <img src="./imagenes/user.jpg" alt=""/></li>
-                </ul>
-            </nav>
-        </header>
+        
+    </nav>
+
+
+
+    </header>
        
     )
-} 
+}
 
-export default Header
+export default Header;
