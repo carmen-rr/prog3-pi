bbf013810ec901cab7069eafe89bd08a59adf99a
tsanzbelloni <tsanzbelloni@udesa.edu.ar>
2022-09-12 12:49:52 -0300

    details

--- proyecto-integrador1/src/screens/AlbumDetail/albumDetail.js
+++ proyecto-integrador1/src/screens/AlbumDetail/albumDetail.js
@@ -1,32 +1,60 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import CardPadre from "../../components/CardPadre/CardPadre"
 
-class AlbumDetail extends Component{
-    constructor(props){
-        super (props)
-        this.state={
-            id: props.match.params.id, 
-            character: {}
+class AlbumDetail extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: props.match.params.id,
+            topAlbums: [],
+            readyAlbums: true,
+
 
         }
     }
 
-    componentDidMount(){
-        fetch(`https://rickandmortyapi.com/api/character/${this.state.id}`)
-        .then(resp => resp.json())
-        .then(data => this.setState({
-            character: data
-        }) )
-        .catch(err => console.log(err))
+    componentDidMount() {
+        fetch(`https://thingproxy.freeboard.io/fetch/https://api.deezer.com/album/${this.state.id}`)
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    topAlbums: data,
+                    readyAlbums: false,
+
+                })
+            })
+            .catch(err => console.log(err))
+
     }
 
 
-    render (){
-
+    render() {
+        console.log(this.state.topAlbums.artist);
         return (
-                <img src={this.state.character.image}/>
+            <>
+
+                {this.state.readyAlbums ?
+                <p>Cargando</p>
+                :
+                    <>
+                        <img src={this.state.topAlbums.cover} />
+                        <p>{this.state.topAlbums.title}</p>
+                        <p>{this.state.topAlbums.artist.name}</p>
+                        <p>{this.state.topAlbums.generes}</p>
+                        <p>{this.state.topAlbums.release_date}</p>
+
+                        {this.state.topAlbums.tracks.map((opcion, idx) =><li> {this.state.topAlbums.tracks}</li>)}
+
+                    </>
+                   
+
+                }
+            </>
         )
 
     }
-} 
+}
 
 export default AlbumDetail
