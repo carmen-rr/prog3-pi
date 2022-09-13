feda4307c90f201fb1add834c11873dac39d3fbb
carmen-rr <crodriguezreynoso@udesa.edu.ar>
2022-09-13 16:19:13 -0300

    favoritos .... ya casi(me trae la info pero no la imprime)

--- proyecto-integrador1/src/screens/Favorites/Favorites.js
+++ proyecto-integrador1/src/screens/Favorites/Favorites.js
@@ -1,24 +1,57 @@
-{/*import React, {Component} from 'react'
+import React , {Component}from 'react'
+import { Link } from 'react-router-dom'
+import CardPadre from "../../components/CardPadre/CardPadre"
 
-
-class Favoritos extends Component {
+class Favorites extends Component {
     constructor(props){
         super(props)
         this.state = {
-            arrayFavs:[]
+            myTracks:[], //array de objetos literales con cada personaje 
+            readySong:false,
+            readyAlbums: false,
         }
     }
-componentDidMount(){
-    let storage = localStorage.getItem('favoritos')
-    if(storage !== null ){
-        let parsedStorage = JSON.parse(storage)
-    }
+
+componentDidMount(){ //lo usamos para que el quitar favoritos si ya esta en el storage el objeto siga
+    let tracksFavoritos = [];
+    let recuperoStorage = localStorage.getItem('tracksFavoritos')
+
+    if (recuperoStorage !== null){ //si hay datos en el storage (algo diferente de null, ya se cargaron datos en el storage)
+        tracksFavoritos = JSON.parse(recuperoStorage); //un array de ids
+       
+        //recorriendo el array y pidiendole los datos de cada personaje 
+
+        let topCanciones = []
+
+        tracksFavoritos.map((id) => {
+
+
+            //pedir por cada id los datos del favorito
+            let url = `https://thingproxy.freeboard.io/fetch/https://api.deezer.com/track/${id}`
+            fetch(url)
+            .then(res => res.json())
+            .then(data => topCanciones.push(data))
+            .catch(err => console.log(err))
+        
+        })
+    console.log(topCanciones)
+}
 }
 
 render(){
     return (
+        <React.Fragment>
         <div>Tus favoritos</div>
+        { 
+            this.state.ready ? //if ternario
+                <>
+                <CardPadre info = {this.state.topCanciones} songs = {true}/>
+                </> : 
+            'Cargando...'
+            }
+            
+        </React.Fragment>
     )
 }
 }
-export default Favorites*/}
+export default Favorites; 
